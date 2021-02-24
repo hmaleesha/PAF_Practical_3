94ba56da2dfe340f72a295359bca585c8f840a19
hmaleesha <hirunimaleesajayasinghe@gmail.com>
2021-02-24 20:15:31 +0530

    Initial commit

---
+++ Labsheet3DB/src/com/Item.java
@@ -0,0 +1,64 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Item {
+
+	public Connection connect() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/itemdb", "root", "");
+		
+		    System.out.println("Successfully connected");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public String insertItem(String code,String name, String price,String desc) {
+		
+		String output = "";
+		
+		try {
+		Connection con = connect();
+		
+		if(con == null) {
+			
+			return "Error while connecting to the database";
+			
+		}
+		
+		String query = "insert into items ('itemID','itemCode','itemName','itemPrice','itemDesc')"+" values(?,?,?,?,?)";
+		
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		
+		preparedStmt.setInt(1, 0);
+		preparedStmt.setString(2, code);
+		preparedStmt.setString(3, name);
+		preparedStmt.setDouble(4, Double.parseDouble(price));
+		preparedStmt.setString(5, desc);
+		
+		preparedStmt.execute();
+		con.close();
+		
+		output = "Inserted successfully";
+		
+		}
+		
+		catch(Exception e){
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+}
